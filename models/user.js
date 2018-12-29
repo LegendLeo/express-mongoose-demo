e387067ba6e4a61b28549819866ef3763d3bb5f7
huqing1473 <huqing1473@emao.com>
2018-12-29 19:46:27 +0800

    :sparkles: 增加登录注册

--- models/user.js
+++ models/user.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const SALT_WORK_FACTOR = 10
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-  fisrt_name: { type: String, required: true, max: 50 },
-  last_name: { type: String, required: true, max: 50 },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 50
+  },
+  password: {
+    type: String,
+    required: true
+  },
   birthday: { type: Date },
   meta: {
     createTime: {
@@ -18,25 +28,37 @@ const UserSchema = new Schema({
 })
 
 UserSchema.pre('save', next => {
+  let user = this
   if (this.isNew) {
     this.meta.createTime = this.meta.updateTime = Date.now()
   } else {
     this.meta.updateTime = Date.now()
   }
-  next()
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) {
+      return next(err)
+    } else {
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) {
+          return next(err)
+        } else {
+          user.password = hash
+          next()
+        }
+      })
+    }
+  })
 })
 
 UserSchema.statics = {
-  fetch (cb) {
-    return this
-      .find()
+  fetch(cb) {
+    return this.find()
       .sort({})
       .exec(cb)
   },
-  findById (id, cb) {
-    return this
-      .findOne({ _id: id })
-      .exec()
+  findById(id, cb) {
+    return this.findOne({ _id: id }).exec()
   }
 }
 
